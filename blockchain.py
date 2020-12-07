7ccaf5a842c9ead3864f222f81520e86a42eb426
hp396 <48489886+hp396@users.noreply.github.com>
2020-12-07 07:15:05 -0500

    edits proof and double spending check

--- blockchain.py
+++ blockchain.py
@@ -5,6 +5,7 @@ from time import time
 class Blockchain(object):
     chain = []
     current_transactions = []
+    address_spend = {}
 
     def __init__(self):
         #initializing a single block to start with
@@ -24,13 +25,41 @@ class Blockchain(object):
 
         self.chain.append(block)
         return block
-    #
-    def new_transaction(self, sender, recipient, amount):
+    
+    #Edits a block of the chain
+    #NOTE: Editing a block of chain's proof will result in wrong value of proof for all the following blocks.
+    def editchain(self,proof,index):
+        for i in self.chain:
+            if i["index"] == index:
+                # if sender != None:
+                #     if len(i["transactions"]) != 0:
+                #         i["transactions"][0]['sender'] = sender
+                if proof != None:
+                    i["proof"] = proof
+                # if recipient != None:
+                #     if len(i["transactions"]) != 0:
+                #         i["transactions"][0]['recipient'] = recipient
+                i["timestamp"] = time()    
+        return 'transaction does not exist',400
+
+    def new_transaction(self, sender, recipient,amount):
         self.current_transactions.append({
             'sender': sender,
             'recipient': recipient,
             'amount': amount,
         })
+        
+        #Edits to the address_spend which keeps track of balance for each address
+        if sender != "0" and sender in self.address_spend:
+            self.address_spend[sender] -= amount
+        elif sender != "0":
+            self.address_spend[sender] = amount
+                
+        if recipient in self.address_spend:
+            self.address_spend[recipient] += amount
+        else:
+            self.address_spend[recipient] = amount
+        print(self.address_spend)
 
         return self.last_block['index'] + 1
 
@@ -54,33 +83,50 @@ class Blockchain(object):
         proof = 0
         while self.valid_proof(last_proof, proof) is False:
             proof += 1
-
         return proof
 
+    def helper_transaction(self,sender, send_amount):
+        amount = 0
+        for i in blockchain.current_transactions:
+            if i["recipient"] == sender:
+                amount += i["amount"]
+            if i["sender"] == sender:
+                amount -= i["amount"]
+        if amount >= send_amount:
+            return True
+        else:
+            return False
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from textwrap import dedent
 from uuid import uuid4
 
 app = Flask(__name__)
-
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 node_identifier = str(uuid4()).replace('-', '')
 
 blockchain = Blockchain()
 
+#TODO: Delete this before submitting, temp method to get values
+@app.route('/spend',methods=['GET'])
+def getspend():
+    return jsonify(blockchain.address_spend)
+        
 @app.route('/mine', methods=['GET'])
 def mine():
-    # We run the proof of work algorithm to get the next proof...
     last_block = blockchain.last_block
     last_proof = last_block['proof']
     proof = blockchain.proof_of_work(last_proof)
 
     # We must receive a reward for finding the proof.
     # The sender is "0" to signify that this node has mined a new coin.
+    sender = "0"
+    recipient = node_identifier
+    
     blockchain.new_transaction(
-        sender="0",
-        recipient=node_identifier,
-        amount=1,
+        sender=sender,
+        recipient=recipient,
+        amount = 1
     )
 
     # Forge the new Block by adding it to the chain
@@ -96,21 +142,35 @@ def mine():
     }
     return jsonify(response), 200
 
+# TODO: error check in front-end any values missing 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    values = request.get_json()
+    values = request.get_json(force=True)
 
-    # Check that the required fields are in the POST'ed data
-    required = ['sender', 'recipient', 'amount']
-    if not all(k in values for k in required):
-        return 'Missing values', 400
+    sender = values["sender"]
+    recipient = values["recipient"]
+    #Checks if sender is not the receiver
+    if sender == recipient:
+        return 'sender cannot be the recipient', 400
 
-    # Create a new Transaction
-    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
-
-    response = {'message': f'Transaction will be added to Block {index}'}
-    return jsonify(response), 201
-
+    if "amount" in values:
+        amount = values["amount"]
+    else:
+        amount = 1
+    
+    #Added to take care of double spend
+    #Checks that the sender has sufficient balance to even send
+        
+    if sender in blockchain.address_spend and blockchain.address_spend[sender] >= amount:
+        index = blockchain.new_transaction(
+            sender=sender,
+            recipient=recipient,
+            amount = amount
+        )
+        response = {'message': f'Transaction will be added to Block {index}'}
+        return jsonify(response), 201
+    else:
+        return 'Recipient does not have sufficient amount to make transaction, recipient has either already sent all the currency or it is in process'
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
@@ -120,5 +180,46 @@ def full_chain():
     }
     return jsonify(response), 200
 
+import os
+import pathlib
+
+#Example code for connecting it with front-end html
+# @app.route('/')
+# def homepage():
+#     abspath = os.path.abspath(__file__)
+#     dname = os.path.dirname(abspath)
+#     os.chdir(dname)
+#     return render_template('index.html')
+
+#TODO: add error check in front-end to make sure that blockno and one other parameter is provided
+@app.route('/edit',methods=['POST'])
+def edit_chain():
+    values = request.get_json(force=True)
+    block_no = values['blockno']
+    if block_no > len(blockchain):
+        return 'Missing value, Need at least one value to edit for any block', 400
+    #Need to think a better way to edit transaction cases.
+    # if "sender" in values:
+    #     sender = values['sender']
+    # else:
+    #     sender = None
+    if "proof" in values:
+        proof = values['proof']
+    else:
+        proof = None
+    # if "recipient" in values:
+    #     recipient = values['recipient']
+    # else:
+    #     recipient = None
+    output = blockchain.editchain(proof,block_no)
+    if output == None:
+        response = {
+            'chain': blockchain.chain,
+            'length': len(blockchain.chain),
+        }
+        return jsonify(response), 201
+    else:
+        output
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='127.0.0.1', port=8080)
